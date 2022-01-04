a44e2ac3f3044c33216414510f955617e7140f2e
vijaykumawat256 <37927929+vijaykumawat256@users.noreply.github.com>
2022-01-03 18:59:18 -0700

    Time: 6 ms (48.08%), Space: 42.6 MB (9.89%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -4,12 +4,14 @@ class Solution {
         Map<Integer, Integer> m = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int diff = target - nums[i];
+            
             if (m.containsKey(diff)) {
-                res[0]=i;
-                res[1]=m.get(diff);
+                res[0]=m.get(diff);
+                res[1]=i;
                 return res;
             }
             m.put(nums[i],i);
+            
             
         }
         
