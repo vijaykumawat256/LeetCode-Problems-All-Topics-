046adeb2717b0197925d65950126ff4b6fb8f8c5
vijaykumawat256 <37927929+vijaykumawat256@users.noreply.github.com>
2022-01-16 11:15:10 -0700

    Time: 3 ms (22.33%), Space: 39.2 MB (50.30%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.java
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.java
@@ -1,11 +1,10 @@
 class Solution {
     int sum=0;
     public int titleToNumber(String columnTitle) {
-        int len=columnTitle.length();
         int j=0;
-       for(int i=len-1;i>=0;i--){
-           char var=columnTitle.charAt(i);
-           int num=(int) var;
+       for(int i=columnTitle.length()-1;i>=0;i--){
+         
+           int num=(int) columnTitle.charAt(i);
            sum=sum+(num-64)*(int)(Math.pow(26,j));
            j++;
            
