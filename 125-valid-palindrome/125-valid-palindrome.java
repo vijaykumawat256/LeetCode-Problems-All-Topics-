0314d8479b32ab8674d314033dff24a8bebc88f8
vijaykumawat256 <37927929+vijaykumawat256@users.noreply.github.com>
2022-01-19 18:42:12 -0700

    Time: 118 ms (12.34%), Space: 41.1 MB (20.13%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+     s=s.replaceAll("[^a-zA-Z0-9]+", "");
+      s=s.toLowerCase();
+        System.out.println(s);
+   String str1=" ";
+        String str2=" ";
+        
+    for(int i=-1+s.length()/2;i>=0;i--){
+        str1=str1+s.charAt(i);
+    }
+    if(s.length()%2==0){
+        for(int i=s.length()/2;i<s.length();i++){
+        str2=str2+s.charAt(i);
+    }
+      
+    }
+        else{
+             for(int i=1+s.length()/2;i<s.length();i++){
+        str2=str2+s.charAt(i);
+        }
+          
+            
+    }
+        System.out.println(str1);
+        System.out.println(str2);
+          if(str1.equals(str2)){
+            return true;
+        }
+        else
+            
+        return false;
+    }
+}
