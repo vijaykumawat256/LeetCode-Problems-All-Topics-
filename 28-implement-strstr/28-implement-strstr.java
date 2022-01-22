f45d1d2322bbfd8cbf0f4a69ec1303795caa5824
vijaykumawat256 <37927929+vijaykumawat256@users.noreply.github.com>
2022-01-21 19:38:05 -0700

    Time: 485 ms (57.56%), Space: 141.3 MB (8.90%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length()==0){
+            return 0;
+        }
+        if(needle.length()>haystack.length()|| needle==null || haystack==null){
+            return -1;
+        }
+        
+        int  need=needle.length();
+        
+        for(int i=0;i<haystack.length()-need+1;i++){
+             if(haystack.charAt(i)==needle.charAt(0)){
+                 if(haystack.substring(i,needle.length()+i).equals(needle)){
+                     return i;
+                 }
+             }
+                
+            
+        }
+   
+        return -1;
+        
+    }
+}
