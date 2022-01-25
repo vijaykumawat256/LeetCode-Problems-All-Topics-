51d8f27104cdc9db8d018b18be72db498deb17ac
vijaykumawat256 <37927929+vijaykumawat256@users.noreply.github.com>
2022-01-24 18:51:48 -0700

    Time: 3 ms (27.48%), Space: 51.3 MB (39.75%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+      
+        int i=0;
+        if(arr.length<3){
+            return false;
+        }
+       while(i<arr.length&& i+1<arr.length &&arr[i]<arr[i+1] && arr[arr.length-1]<arr[arr.length-2]){
+  
+           i++;
+       }
+         while(i<arr.length && i+1<arr.length && arr[i]>arr[i+1]&& arr[0]<arr[1]){
+         
+             i++;
+       }
+        
+        
+        if(i==arr.length-1){
+            return true;
+        }
+        
+        else
+            return false;
+        
+        
+    }
+}
